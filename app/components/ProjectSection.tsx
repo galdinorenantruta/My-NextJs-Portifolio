bc87fa361f32a78911ec1ba6c92ecc305ebb8a69
Renan Galdino Truta <115110286+galdinorenantruta@users.noreply.github.com>
2023-08-04 18:54:52 -0300

    fixed project comp

--- app/components/ProjectSection.tsx
+++ app/components/ProjectSection.tsx
@@ -7,7 +7,7 @@ const projects = [
   {
     name: "Ecommerce Website",
     description:
-      "I've used ReactStrap for styling, ReduxToolKit for functionalities, NodeJs as Backend and FireBase as DB.",
+      "full stack react ecommerce website using reactjs, redux toolkit and firebase, with modern UI and UX",
     image: "/ecommerce.png",
 
     link: "https://www.linkedin.com/posts/renan-galdino_reactjs-reduxtoolkit-reactstrap-activity-7081635819119083520-Qdxa?utm_source=share&utm_medium=member_desktop",
@@ -42,35 +42,33 @@ const ProjectsSection = () => {
         {projects.map((project, idx) => {
           return (
             <div key={idx}>
-              
-                <div className="flex flex-col md:flex-row md:space-x-12">
-                  <div className=" md:w-1/2">
-                    <Link href={project.link}>
-                      <Image
-                        src={project.image}
-                        alt=""
-                        width={1000}
-                        height={1000}
-                        className="rounded-xl shadow-xl hover:opacity-70"
+              <div className="flex flex-col md:flex-row md:space-x-12">
+                <div className=" md:w-1/2">
+                  <Link href={project.link}>
+                    <Image
+                      src={project.image}
+                      alt=""
+                      width={1000}
+                      height={1000}
+                      className="rounded-xl shadow-xl hover:opacity-70"
+                    />
+                  </Link>
+                </div>
+                <div className="mt-8 md:w-1/2">
+                  <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                  <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                    {project.description}
+                  </p>
+                  <div className="flex flex-row align-bottom space-x-4">
+                    <Link href={project.link} target="_blank">
+                      <BsArrowUpRightSquare
+                        size={30}
+                        className="hover:-translate-y-1 transition-transform cursor-pointer"
                       />
                     </Link>
                   </div>
-                  <div className="mt-8 md:w-1/2">
-                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
-                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                      {project.description}
-                    </p>
-                    <div className="flex flex-row align-bottom space-x-4">
-                      <Link href={project.link} target="_blank">
-                        <BsArrowUpRightSquare
-                          size={30}
-                          className="hover:-translate-y-1 transition-transform cursor-pointer"
-                        />
-                      </Link>
-                    </div>
-                  </div>
                 </div>
-             
+              </div>
             </div>
           );
         })}
