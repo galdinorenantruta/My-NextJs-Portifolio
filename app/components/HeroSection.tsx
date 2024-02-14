4ecda4f2436a11b2e55692e4aac9fc9a33009479
galdinorenantruta <galdinotrutarenan@gmail.com>
2024-02-14 09:25:52 -0300

    fix

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -46,16 +46,9 @@ const HeroSection = () => {
         </div>
       </div>
       <div className="flex flex-row items-center text-center justify-center ">
-        <Link
-          to="about"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
+        
           <HiArrowDown size={35} className="animate-bounce" />
-        </Link>
+        
       </div>
     </section>
   );
