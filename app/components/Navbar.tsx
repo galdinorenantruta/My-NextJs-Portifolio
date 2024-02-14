967d9bafbd9945678400762e2763c388f06926f5
galdinorenantruta <galdinotrutarenan@gmail.com>
2024-02-14 09:31:24 -0300

    fix2

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-scroll/modules";
-import { usePathname } from "next/navigation";
+
 import { useTheme } from "next-themes";
 import { RiMoonFill, RiSunLine } from "react-icons/ri";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
@@ -30,18 +30,18 @@ const NAV_ITEMS: Array<NavItem> = [
 export default function Navbar() {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
-  const pathname = usePathname();
+
   const [navbar, setNavbar] = useState(false);
   return (
     <header className=" w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark?:border-stone-600">
       <div className="justify-between md:items-center md:flex ">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link to="home">
+            
               <div className="container flex items-center space-x-2 cursor-pointer">
                 <h2 className="text-2xl font-bold ">Renan Galdino</h2>
               </div>
-            </Link>
+           
             <div className="md:hidden">
               <button
                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
